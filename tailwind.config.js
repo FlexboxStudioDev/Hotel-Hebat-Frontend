68fce064b67f3bc7ec4c106c4ee9ba4915d176ff
Faris Fadhail <farisfadhail@gmail.com>
2022-07-14 09:17:38 +0700

    index.html - Done & Add Facility.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["./src/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
 	theme: {
 		extend: {
 			colors: {
@@ -8,5 +8,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("daisyui")],
+	plugins: [require("daisyui"), require("tw-elements/dist/plugin")],
 };
